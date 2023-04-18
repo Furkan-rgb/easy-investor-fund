1e24069846691f9f1d4d0c22fd3c503fe19dd896
Öztürk,Furkan F <50831308+Furkan-rgb@users.noreply.github.com>
2023-04-18 23:13:39 +0200

    Update StyledCard.tsx

--- core/components/StyledCard.tsx
+++ core/components/StyledCard.tsx
@@ -4,10 +4,10 @@ import Card from "./Card";
 export default function StyledCard({ children }: { children?: React.ReactNode }) {
   return (
     <div className="relative grid">
-      <Card className="z-10 transition hover:translate-x-0.5 hover:-translate-y-0.5">
+      <div className="z-10 p-4 transition bg-white border-2 rounded-sm hover:translate-x-1 hover:-translate-y-1 border-gray-950">
         <div className="z-20 prose">{children}</div>
-      </Card>
-      <Card className="absolute inset-0 z-0 w-full h-full bg-black" />
+      </div>
+      <div className="absolute inset-0 z-0 w-full h-full bg-black rounded-sm" />
     </div>
   );
 }
